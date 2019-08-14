d494110f8cd607e5505abb9e8b1711ef44ae6273
Paty Alves <patriciaalves10@gmail.com>
2019-08-13 21:46:29 -0300

    create custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
+  forbiddenUserName= ['paty', 'fulano', 'ciclano'];
+
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username' : new FormControl(null, Validators.required),
+        'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('female'),
@@ -34,6 +36,14 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean} {
+    if ( this.forbiddenUserName.indexOf(control.value) !== -1 ) {
+      return { 'nameIsForbidden': true };
+    }
+    return null;
+
   }
 
 
